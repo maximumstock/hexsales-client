5a17535c7c090827cb0d903922a937fd5f123599
Maximilian Stock <me@maximumstock.net>
2016-04-26 18:10:52 +0200

    less double bindings, partial jquery dom manip test

--- app/scripts/controllers/articleCtrl.js
+++ app/scripts/controllers/articleCtrl.js
@@ -5,14 +5,29 @@ app.controller('ArticleCtrl', ['$scope', '$location', 'Api', function($scope, $l
   // grab all articles
   Api.searchArticles({like: true, limit: 10000})
     .then(function(res) {
-      $scope.allArticles = res.data.filter(function(a) {
-        return a.name !== '';
-      });
-      $scope.allSortedArticles = {
-        cards: $scope.allArticles.filter(function(e) { return e.type === 'Card'; }),
-        equipment: $scope.allArticles.filter(function(e) { return e.type === 'Equipment'; }),
-        others: $scope.allArticles.filter(function(e) { return e.type !== 'Card' && e.type !== 'Equipment'; })
-      };
+     
+			var relevantArticles = res.data.filter(function(a) {
+				return a.name !== '';
+			});
+
+			// append elements
+			var cards = relevantArticles.filter(function(a) {
+				return a.type === 'Card';
+			});
+			var cardList = document.getElementById('card-list');
+			cards.forEach(function(c) {
+				
+				$(cardList).append('<li><a href="#/articles/' + c.name + '">' + c.name + ' [ ' + c.rarity[0] + ' - ' + c.setid + ' ]' + '</a></li>');
+
+			});
+			$scope.allArticles = res.data.filter(function(a) {
+			  return a.name !== '';
+			});
+			$scope.allSortedArticles = {
+			  // cards: $scope.allArticles.filter(function(e) { return e.type === 'Card'; }),
+			  equipment: $scope.allArticles.filter(function(e) { return e.type === 'Equipment'; }),
+			  others: $scope.allArticles.filter(function(e) { return e.type !== 'Card' && e.type !== 'Equipment'; })
+			};
     })
     .catch(function(err) {
       throw err;
