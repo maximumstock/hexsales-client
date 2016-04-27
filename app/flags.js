7ea329fe89ec0167a41c3bced6bb280f1c01f1b4
Maximilian Stock <me@maximumstock.net>
2016-04-27 09:09:21 +0200

    fix chart flag typo

--- app/flags.js
+++ app/flags.js
@@ -5,7 +5,7 @@ var chartFlags = [
   {x: Date.parse('2015-03-10'), text: '<a target="_blank" href="https://www.hextcg.com/hex-update-arena-opens-march-10th">Arena Launch</a>', title: 'Arena Launch'},
   {x: Date.parse('2015-07-21'), text: '<a target="_blank" href="#/articles/Set 003 Booster Pack">Armies of Myth</a>', title: 'AoM Launch'},
   {x: Date.parse('2016-01-29'), text: '<a target="_blank" href="https://www.hextcg.com/hex-update-entraths-alive">PvE Campaign Launch</a>', title: 'PvE Campaign Launch'},
-	{x: Date.parse('2016-04-26'), text: '<a target="_blank" href="https://www.hextcg.com/primal-dawn-is-now-live">Primal Dawn Launch</a>', title: 'PD Launch}
+	{x: Date.parse('2016-04-26'), text: '<a target="_blank" href="https://www.hextcg.com/primal-dawn-is-now-live">Primal Dawn Launch</a>', title: 'PD Launch'}
 ];
 
 var chartFlagSeries = {
