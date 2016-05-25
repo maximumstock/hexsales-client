5d43bcdd599f3579e32dab106dd4d07968081e38
Maximilian Stock <me@maximumstock.net>
2016-05-25 17:38:13 +0200

    card portrait now toggleable on demand

--- app/scripts/directives/cardimg.js
+++ app/scripts/directives/cardimg.js
@@ -7,19 +7,48 @@ angular.module('hexsales-client')
         name: '@'
       },
       template: '<div> \
-                    <div class="portrait-container card-portrait"> \
+					<button class="btn btn-default" id="toggleCardPortraitButton">Toggle Card Portrait</button> \
+                    <div class="card-portrait"> \
                     </div> \
                 </div>',
       link: function(scope, element, attrs) {
-        $('.portrait-container').hide();
+        $('.card-portrait').hide();
 
-			// load picture from gameforge
-			var imgUrl = 'https://cards.hex.gameforge.com/cardsdb/en/' + scope.name + '.png';
-			var $img = $('<img src="' + imgUrl + '" />')
-						.on('error', function() {return;})
-						.on('load', function() {
-							$('.card-portrait').append($img).show();
-						});
+		var portraitLoaded = false;
+		var portraitVisible = false;
+
+		$('#toggleCardPortraitButton').click(toggleCardPortrait);
+
+		function toggleCardPortrait() {
+		
+			if(portraitLoaded === false) {
+				// need to load card portrait for first time
+				// load picture from gameforge
+				var imgUrl = 'https://cards.hex.gameforge.com/cardsdb_350x490/en/' + scope.name + '.png';
+				var $img = $('<img src="' + imgUrl + '" />')
+							.on('error', function() {
+								$('.card-portrait').append('<p>There was a problem loading the portrait</p>').show();
+								portraitLoaded = true;
+							})
+							.on('load', function() {
+								$('.card-portrait').append($img).show();
+								portraitVisible = true;
+								portraitLoaded = true;
+							});
+			} else {
+				if(portraitVisible === false) {
+					$('.card-portrait').show();
+					portraitVisible = true;
+				} else {
+					$('.card-portrait').hide();
+					portraitVisible = false;
+				}
+			}
+		
+			
+		
+		}
+
 
 				// var imgHost = "http://migration.tcgbrowser.com/files/full-size-jpg/";
 
