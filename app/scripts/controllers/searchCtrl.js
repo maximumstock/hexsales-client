3711ab8f27a015f30652687a22a77057cd445d91
Maximilian Stock <me@maximumstock.net>
2016-05-25 17:54:08 +0200

    added search field auto complete

--- app/scripts/controllers/searchCtrl.js
+++ app/scripts/controllers/searchCtrl.js
@@ -10,4 +10,17 @@ angular.module('hexsales-client').controller('SearchCtrl', ['$scope', '$location
     $location.path('/search/' + (term || ''));
   };
 
+	// on load: get all articles and add a completion list to the search bar
+	Api.searchArticles({limit: 90000})
+		.then(function(res) {
+			console.log(res.data);
+			res.data.forEach(function(article) {
+				$('#articleNameList').append('<option value="' + article.name + '">');
+			});
+		})
+		.catch(function(err) {
+			console.log(err);
+		});
+
+
 }]);
