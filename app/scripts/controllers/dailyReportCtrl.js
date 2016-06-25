dc570ff14d05340fa1e3aeca53c54ee99f1681df
Maximilian Stock <me@maximumstock.net>
2016-06-26 01:34:57 +0200

    increased number of articles in the most sold articles view of daily report

--- app/scripts/controllers/dailyReportCtrl.js
+++ app/scripts/controllers/dailyReportCtrl.js
@@ -35,7 +35,7 @@ angular.module('hexsales-client')
     getMostSoldArticles({
       start: moment().subtract(3, 'days').format('YYYY-MM-DD'),
       end: moment().format('YYYY-MM-DD'),
-      limit: 60
+      limit: 100
     });
 
 
