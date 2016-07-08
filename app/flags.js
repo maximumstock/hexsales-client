3ec32cda2f5a9f55ef3272ead0791bb5cf2f834f
Maximilian Stock <me@maximumstock.net>
2016-07-08 13:38:15 +0200

    update chart flags (added ladder launch date)

--- app/flags.js
+++ app/flags.js
@@ -3,12 +3,13 @@
 function getFlagSeries() {
 
 	var chartFlags = [
-	  // {x: Date.parse('2014-12-09'), text: '<a target="_blank" href="#/articles/Set 002 Booster Pack">Shattered Destiny Launch</a>', title: 'SD Launch'},
-	  {x: Date.parse('2015-03-10'), text: '<a target="_blank" href="https://www.hextcg.com/hex-update-arena-opens-march-10th">Arena Launch</a>', title: 'Arena Launch'},
-	  {x: Date.parse('2015-07-21'), text: '<a target="_blank" href="#/articles/Set 003 Booster Pack">Armies of Myth</a>', title: 'AoM Launch'},
-	  {x: Date.parse('2016-01-29'), text: '<a target="_blank" href="https://www.hextcg.com/hex-update-entraths-alive">PvE Campaign Launch</a>', title: 'PvE Campaign Launch'},
-		{x: Date.parse('2016-04-26'), text: '<a target="_blank" href="https://www.hextcg.com/primal-dawn-is-now-live">Primal Dawn Launch</a>', title: 'PD Launch'}
-	];
+	    // {x: Date.parse('2014-12-09'), text: '<a target="_blank" href="#/articles/Set 002 Booster Pack">Shattered Destiny Launch</a>', title: 'SD Launch'},
+	    {x: Date.parse('2015-03-10'), text: '<a target="_blank" href="https://www.hextcg.com/hex-update-arena-opens-march-10th">Arena Launch</a>', title: 'Arena Launch'},
+	    {x: Date.parse('2015-07-21'), text: '<a target="_blank" href="#/articles/Set 003 Booster Pack">Armies of Myth</a>', title: 'AoM Launch'},
+	    {x: Date.parse('2016-01-29'), text: '<a target="_blank" href="https://www.hextcg.com/hex-update-entraths-alive">PvE Campaign Launch</a>', title: 'PvE Campaign Launch'},
+	    {x: Date.parse('2016-04-26'), text: '<a target="_blank" href="https://www.hextcg.com/primal-dawn-is-now-live">Primal Dawn Launch</a>', title: 'PD Launch'},
+        {x: Date.parse('2016-07-05'), text: '<a target="_blank" href="https://www.hextcg.com/ladder-patch-notes">Ladder Launch</a>', title: 'Ladder Launch'}
+    ];
 	
 	var chartFlagSeries = {
 	  type: 'flags',
