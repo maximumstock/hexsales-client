59607859e18841bfb18d220bcc849077d044968a
Maximilian Stock <me@maximumstock.net>
2016-09-16 16:29:49 +0200

    added live color-switchting to favorite heart buttons

--- app/scripts/directives/favlist.js
+++ app/scripts/directives/favlist.js
@@ -7,15 +7,29 @@ angular.module('hexsales-client')
         name: '@',
         uuid: '@'
       },
-      template: '<button class="btn btn-default btn-xs follow-button">Favorite <span style="color: rgb(220, 20, 20);" class="glyphicon glyphicon-heart"></span></button>',
+      template: '<button class="btn btn-default btn-xs favorite-button">Favorite <span class="glyphicon glyphicon-heart"></span></button>',
       link: function(scope, element, attrs) {
+
+        // if the current article already exists in a user's favorites, then change the heart color to indicate missing
+        // favorization
+        var favorites = $cookies.getObject('hexsales-article-favorites') || [];
+
+        var duplicates = favorites.filter(function(e) {
+          return e.uuid === scope.uuid;
+        });
+        console.log(duplicates.length);
+        if(duplicates.length !== 0) {
+            console.log($(element).children());
+            element.children().addClass('favorite-button-favorited');
+            console.log("lol");
+        }
 
         // when the button is clicked, add the name of the article to the list in the cookie
         $(element).click(function(event) {
 
-            var favorites = $cookies.getObject('hexsales-article-favorites') || [];
+            favorites = $cookies.getObject('hexsales-article-favorites') || favorites;
 
-            var duplicates = favorites.filter(function(e) {
+            duplicates = favorites.filter(function(e) {
                 return e.uuid === scope.uuid;
             });
 
@@ -24,11 +38,13 @@ angular.module('hexsales-client')
                     name: scope.name,
                     uuid: scope.uuid
                 });
+                element.children().addClass('favorite-button-favorited')
             } else {
                 // if the article already exists in the favorites array then remove it
                 favorites = favorites.filter(function(e) {
                     return e.uuid !== scope.uuid;
                 });
+                element.children().removeClass('favorite-button-favorited');
             }
             
             $cookies.putObject('hexsales-article-favorites', favorites);
