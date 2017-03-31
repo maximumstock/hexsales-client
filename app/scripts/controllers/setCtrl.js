c31b3e7d41e72a174c4a4cef067ca7c68258305c
Maximilian Stock <me@maximumstock.net>
2017-04-01 00:51:09 +0200

    everything basically; improved set pages 1) brought back summaries by rarity, 2) set article list with price data

--- app/scripts/controllers/setCtrl.js
+++ app/scripts/controllers/setCtrl.js
@@ -2,119 +2,143 @@
 
 angular.module('hexsales-client').controller('SetCtrl', ['$scope', '$location', '$routeParams', 'NgTableParams', 'Api', function($scope, $location, $routeParams, NgTableParams, Api) {
 
-    $scope.setName = $routeParams.set;
-    $scope.realSetName = Api.findRealSetName($scope.setName);
-    // pre-init
-    $scope.articles = [];
-    $scope.summaryByRarityData = [];
-    $scope.summaryData = {};
-    $scope.historyData = {};
-    $scope.chartConfigs = {
-        platinum: {},
-        gold: {}
-    };
+	$scope.setName = $routeParams.set;
+	$scope.realSetName = Api.findRealSetName($scope.setName);
+	// pre-init
+	$scope.articles = [];
+	$scope.summaryByRarityData = [];
+	$scope.summaryData = {};
+	$scope.historyData = {};
+	$scope.chartConfigs = {
+		platinum: {},
+		gold: {}
+	};
 
-    getArticles();
+  $scope.pricelist = {};
 
-    // helper function to find all articles for the current set
-    function getArticles() {
-        var params = {
-            set: $scope.setName,
-            limit: 1000
-        };
+	getArticles();
 
-        Api.searchArticles(params)
-            .then(function(res) {
-                $scope.articles = res.data;
-                if($scope.articles.length === 0) {
-                    return $location.path('/about');
-                }
-                getSummaries();
-                // getSummariesByRarity();
-                $scope.articlesTable = new NgTableParams({
-                  sorting: {
-                    'name': 'asc'
-                  }
-                }, {
-                  dataset: $scope.articles
-                });
-            })
-            .catch(function(error) {
-                console.log(error);
-                // @TODO error handling
-            })
-    }
+	// helper function to find all articles for the current set
+	function getArticles() {
+		var params = {
+			set: $scope.setName,
+			limit: 1000
+		};
 
-    // helper function to load summary data by rarity
-    function getSummariesByRarity() {
+		Api.searchArticles(params)
+			.then(function(res) {
+				$scope.articles = res.data;
+				if ($scope.articles.length === 0) {
+					return $location.path('/about');
+				}
+				getSummaries();
+				getSummariesByRarity();
+				$scope.articlesTable = new NgTableParams({
+					sorting: {
+						'name': 'asc'
+					}
+				}, {
+					dataset: $scope.articles
+				});
 
-        var timeframes = [1, 3, 5, 7];
-        timeframes.forEach(function(timeframe) {
-
-            gameRarities.forEach(function(rarity) {
-           
-                var params = {
-                    start: moment().subtract(timeframe, 'days').format('YYYY-MM-DD'),
-                    end: moment().format('YYYY-MM-DD'),
-                    set: $scope.setName,
-                    rarity: rarity
-                };
-          
-                Api.getSummary(params)
-                    .then(function(res) {
-                        $scope.summaryByRarityData.push({
-                            rarity: rarity,
-                            rarityNumeric: Api.mapRarityToInt(rarity), 
-                            timeframe: timeframe,
-                            data: Api.formatAllDemNumbers(res.data)
-                        });
-                        $scope.summaryByRarityData.sort(function(a, b) {
-                            if(a.rarityNumeric < b.rarityNumeric) return -1;
-                            if(a.rarityNumeric > b.rarityNumeric) return 1;
-                            return 0;
-                        });
-                    })
-                    .catch(function(err) {
-                        console.log(err);
-                    });
-
-
-            }); 
-
+        return Api.getPricelist();
+			})
+      .then(function(res) {
+        $scope.pricelist = res.data;
+        $scope.articles.forEach(function(article) {
+          var uuid = article.uuid;
+          // determine most recent keys (1-15) for both currencies and attach found
+          // pricelist data to each article
+          ['platinum', 'gold'].forEach(function(currency) {
+            try {
+              var key = Object.keys($scope.pricelist[currency][uuid])[0];
+              if(key) {
+                article[currency] = $scope.pricelist[currency][uuid][key];
+                Api.formatAllDemNumbers(article[currency])
+                article[currency].key = key;
+              }
+            } catch (e) {
+              return;
+            }
+          });
         });
+      })
+			.catch(function(error) {
+				console.error(error);
+				// @TODO error handling
+			})
+	}
 
-    }
+	// helper function to load summary data by rarity
+	function getSummariesByRarity() {
 
-    // helper function to load summary data
-    function getSummaries() {
+		var timeframes = [1, 3, 5, 7];
+		timeframes.forEach(function(timeframe) {
 
-        // assign helper method to scope so it can be used in templates
-        $scope.isNumber = angular.isNumber;
+			gameRarities.forEach(function(rarity) {
 
-        var timeframes = [1, 2, 3, 6, 7, 8, 13, 14, 15]; // find summaries for last {x} days
-        timeframes.forEach(function(timeframe) {
+				var params = {
+					start: moment().subtract(timeframe, 'days').format('YYYY-MM-DD'),
+					end: moment().format('YYYY-MM-DD'),
+					set: $scope.setName,
+					rarity: rarity
+				};
 
-            var params = {
-                start: moment().subtract(timeframe, 'days').format('YYYY-MM-DD'),
-                end: moment().format('YYYY-MM-DD'),
-                set: $scope.setName
-            };
+				Api.getSummary(params)
+					.then(function(res) {
+						$scope.summaryByRarityData.push({
+							rarity: rarity,
+							rarityNumeric: Api.mapRarityToInt(rarity),
+							timeframe: timeframe,
+							data: Api.formatAllDemNumbers(res.data)
+						});
+						$scope.summaryByRarityData.sort(function(a, b) {
+							if (a.rarityNumeric < b.rarityNumeric) return -1;
+							if (a.rarityNumeric > b.rarityNumeric) return 1;
+							return 0;
+						});
+					})
+					.catch(function(err) {
+						console.error(err);
+					});
 
-            Api.getSummary(params)
-                .then(function(res) {
-                    Object.keys(res.data).forEach(function(cur) {
-                        Object.keys(res.data[cur]).forEach(function(p) {
-                            res.data[cur][p] = numeral(res.data[cur][p] || 0).format('0,0');
-                        })
-                    })
-                    $scope.summaryData[timeframe] = res.data;
-                })
-                .catch(function(err) {
-                    throw err;
-                });
 
-        });
+			});
 
-    }
+		});
+
+	}
+
+	// helper function to load summary data
+	function getSummaries() {
+
+		// assign helper method to scope so it can be used in templates
+		$scope.isNumber = angular.isNumber;
+
+		var timeframes = [1, 2, 3, 6, 7, 8, 13, 14, 15]; // find summaries for last {x} days
+		timeframes.forEach(function(timeframe) {
+
+			var params = {
+				start: moment().subtract(timeframe, 'days').format('YYYY-MM-DD'),
+				end: moment().format('YYYY-MM-DD'),
+				set: $scope.setName
+			};
+
+			Api.getSummary(params)
+				.then(function(res) {
+					Object.keys(res.data).forEach(function(cur) {
+						Object.keys(res.data[cur]).forEach(function(p) {
+							res.data[cur][p] = numeral(res.data[cur][p] || 0).format('0,0');
+						})
+					})
+					$scope.summaryData[timeframe] = res.data;
+				})
+				.catch(function(err) {
+					throw err;
+				});
+
+		});
+
+	}
 
 }]);
